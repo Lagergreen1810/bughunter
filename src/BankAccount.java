7a1cd3719ceb3994d055b3981af661380eceb57c
Sebastian <seha1002@stud.ek.dk>
2026-09-21 11:06:06 +0200

    lavede en BankAccount class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,23 +3,24 @@
 class BankAccount {
     private double balance;
 
-    BankAccount (double balance) {
+    BankAccount(double balance) {
         this.balance = balance;
     }
 }
-double balance = 1000;
+    double balance = 1000;
 
-void main() {
-    BankAccount account = new BankAccount(1000);
-    withdraw(1200);
-    deposit(100);
-    IO.println(balance);
-}
+    void main() {
+        BankAccount account = new BankAccount(1000);
+        withdraw(1200);
+        deposit(100);
+        IO.println(balance);
+    }
 
-void withdraw(double amount) {
-    balance = balance - amount;
-}
+    void withdraw(double amount) {
+        balance = balance - amount;
+    }
 
-void deposit(double amount) {
-    balance = balance + amount;
-}
+    void deposit(double amount) {
+        balance = balance + amount;
+    }
+
